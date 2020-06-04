0832b50adfb076e5da0bdf8e37b7a7afacf6b602
ortal chifrut <ortalchif@gmail.com>
2020-06-04 14:50:54 +0300

    ININT

---
+++ Ex03.ConsoleUI/Vehicle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex03.ConsoleUI
+{
+    class Vehicle
+    {
+
+        public string m_model;
+        public string m_licenseNumber;
+        public void m_energyLeftOver;
+        //מערך של צמיגים
+    }
+}
